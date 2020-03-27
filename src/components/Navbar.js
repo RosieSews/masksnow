fe5b07e8a1bb28913748d3cc64bbf89350ca53fe
Garrett Van Gotten <garrett.van-gotten@c2fo.com>
2020-03-27 03:16:32 -0500

    wip

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Link } from "gatsby";
+import { css } from "styled-components";
 import headerBackground from "../img/header-bg.png";
 import logoImg from "../img/header-rosie.png";
 import logoImgMobile from "../img/header-mobile.png";
 import maskNowImg from "../img/header-masksnow.png";
+import MobileNav from "./MobileNav";
+import { bpMaxSM } from "../lib/breakpoints";
+
+const HamburgerLine = () => {
+  return <span />
+}
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -16,22 +23,13 @@ const Navbar = class extends React.Component {
 
   toggleHamburger = () => {
     // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: "is-active"
-            })
-          : this.setState({
-              navBarActiveClass: ""
-            });
+    this.setState((prevState) => {
+      let active = !prevState.active;
+      return {
+        active,
+        navBarActiveClass: active ? 'is-active' : '',
       }
-    );
+    });
   };
 
   render() {
@@ -45,24 +43,42 @@ const Navbar = class extends React.Component {
             backgroundRepeat: "repeat-x"
           }}
         >
-          <img
-            src={logoImgMobile}
-            alt="blah"
-            className="header-logo-banner"
-          ></img>
-          <img src={logoImg} className="header-logo" alt="We can do it!"></img>
+          <img src={logoImgMobile} alt="blah" className="header-logo-banner" />
+          <img src={logoImg} className="header-logo" alt="We can do it!" />
           <div className="header-container">
             <div className="header-top">
+            {/*  <div*/}
+            {/*      css={css`*/}
+            {/*  font-size: 16px;*/}
+            {/*  line-height: 1.25;*/}
+            {/*  display: flex;*/}
+            {/*  align-items: center;*/}
+            {/*  .mobile-nav {*/}
+            {/*    display: none;*/}
+            {/*    visibility: hidden;*/}
+            {/*    ${bpMaxSM} {*/}
+            {/*      display: block;*/}
+            {/*      visibility: visible;*/}
+            {/*    }*/}
+            {/*  }*/}
+            {/*`}*/}
+            {/*  >*/}
+
               {/* Hamburger menu */}
+              {/*<MobileNav />*/}
               <div
                 className={`navbar-burger burger ${this.state.navBarActiveClass}`}
                 data-target="navMenu"
                 onClick={() => this.toggleHamburger()}
-              ></div>{" "}
+              >
+                <HamburgerLine />
+                <HamburgerLine />
+                <HamburgerLine />
+              </div>{" "}
               {/* Navbar-burger */}
               <div
-                id="navMenu"
-                className={`navbar-menu ${this.state.navBarActiveClass}`}
+                  id="navMenu"
+                  className={`navbar-menu ${this.state.navBarActiveClass}`}
               >
                 <div className="navbar-start has-text-centered">
                   <Link className="navbar-item" to="/about-us">
@@ -89,16 +105,18 @@ const Navbar = class extends React.Component {
                 </div>{" "}
                 {/* Navbar-start */}
               </div>{" "}
+              {/*</div>*/}
+
               {/* navMenu */}
             </div>{" "}
             {/* Container */}
             <div style={{ display: "flex", alignItems: "center", flex: "1" }}>
               <img
                 src={maskNowImg}
-                alt="Blah"
+                alt="masks now banner"
                 class="header-banner"
                 style={{ maxHeight: "228px", width: "100%" }}
-              ></img>
+              />
             </div>
           </div>
         </div>
