ed4acf92fed1264314268fc3c61665e34c951737
mpasalic <mirza.pasalic@gmail.com>
2020-03-27 01:35:52 -0700

    Add call to action buttons.
    Screenshot: https://slack-files.com/T010PQGP4UQ-F010WFH5HFX-f1cdeed04c

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,64 +36,70 @@ const Navbar = class extends React.Component {
 
   render() {
     return (
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div
-          className="header"
-          style={{
-            backgroundImage: "url(" + headerBackground + ")",
-            backgroundSize: "contain",
-            backgroundRepeat: "repeat-x"
-          }}
-        >
-          <img
-            src={logoImgMobile}
-            alt="blah"
-            className="header-logo-banner"
-          ></img>
-          <img src={logoImg} className="header-logo" alt="We can do it!"></img>
-          <div className="header-container">
-            <div className="header-top">
-              {/* Hamburger menu */}
-              <div
-                className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-                data-target="navMenu"
-                onClick={() => this.toggleHamburger()}
-              ></div>{" "}
-              {/* Navbar-burger */}
-              <div
-                id="navMenu"
-                className={`navbar-menu ${this.state.navBarActiveClass}`}
-              >
-                <div className="navbar-start has-text-centered">
-                  <Link className="navbar-item" to="/request-supplies">
-                    Request Supplies
-                  </Link>
-                  <Link className="navbar-item" to="/patterns">
-                    Patterns
-                  </Link>
-                  <Link className="navbar-item" to="/groups-directory">
-                    Groups Directory
-                  </Link>
-                  <Link className="navbar-item" to="/volunteer">
-                    Volunteer
-                  </Link>
+      <div>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div
+            className="header"
+            style={{
+              backgroundImage: "url(" + headerBackground + ")",
+              backgroundSize: "contain",
+              backgroundRepeat: "repeat-x"
+            }}
+          >
+            <img
+              src={logoImgMobile}
+              alt="blah"
+              className="header-logo-banner"
+            ></img>
+            <img src={logoImg} className="header-logo" alt="We can do it!"></img>
+            <div className="header-container">
+              <div className="header-top">
+                {/* Hamburger menu */}
+                <div
+                  className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+                  data-target="navMenu"
+                  onClick={() => this.toggleHamburger()}
+                ></div>{" "}
+                {/* Navbar-burger */}
+                <div
+                  id="navMenu"
+                  className={`navbar-menu ${this.state.navBarActiveClass}`}
+                >
+                  <div className="navbar-start has-text-centered">
+                    <Link className="navbar-item" to="/request-supplies">
+                      Request Supplies
+                    </Link>
+                    <Link className="navbar-item" to="/patterns">
+                      Patterns
+                    </Link>
+                    <Link className="navbar-item" to="/groups-directory">
+                      Groups Directory
+                    </Link>
+                    <Link className="navbar-item" to="/volunteer">
+                      Volunteer
+                    </Link>
+                  </div>{" "}
+                  {/* Navbar-start */}
                 </div>{" "}
-                {/* Navbar-start */}
+                {/* navMenu */}
               </div>{" "}
-              {/* navMenu */}
-            </div>{" "}
-            {/* Container */}
-            <div style={{ display: "flex", alignItems: "center", flex: "1" }}>
-              <img
-                src={maskNowImg}
-                alt="Blah"
-                class="header-banner"
-                style={{ maxHeight: "228px", width: "100%" }}
-              ></img>
+              {/* Container */}
+              <div style={{ display: "flex", alignItems: "center", flex: "1" }}>
+                <img
+                  src={maskNowImg}
+                  alt="Blah"
+                  class="header-banner"
+                  style={{ maxHeight: "228px", width: "100%" }}
+                ></img>
+              </div>
             </div>
           </div>
+        </nav>
+        <div className="call-to-action">
+          <Link to="/volunteer">Volunteer Here</Link>
+          <Link to="/request-supplies">Request Supplies</Link>
         </div>
-      </nav>
+      </div>
     );
   }
 };
