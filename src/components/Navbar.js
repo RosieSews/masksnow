f01e3d3aa426233fda74991e7b60eb64164547c5
mpasalic <mirza.pasalic@gmail.com>
2020-03-26 23:07:20 -0700

    Finish up implementation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import logo from '../img/masksnow.svg'
 import headerBackground from "../img/header-bg.png"
 import logoImg from "../img/header-rosie.png";
 import logoImgMobile from "../img/header-mobile.png";
@@ -38,15 +37,14 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="header" style={{flexDirection: "row", backgroundImage: "url(" + headerBackground + ")", backgroundSize: "contain", backgroundRepeat: "repeat-x"}}>
-          <img src={logoImg} style={{height: "300px"}} alt="We can do it!"></img>
-          <div style={{flexDirection: "column", display: "flex"}}>
-            <div style={{height: "74px"}}>
-            
+        <div className="header" style={{backgroundImage: "url(" + headerBackground + ")", backgroundSize: "contain", backgroundRepeat: "repeat-x"}}>
+          <img src={logoImgMobile} alt="blah" className="header-logo-banner"></img>
+          <img src={logoImg}  className="header-logo" alt="We can do it!"></img>
+          <div className="header-container">
+            <div className="header-top">
               {/* Hamburger menu */}
               <div className={`navbar-burger burger ${this.state.navBarActiveClass}`} data-target="navMenu" onClick={() => this.toggleHamburger()}>
               </div> {/* Navbar-burger */}
-
               <div id="navMenu" className={`navbar-menu ${this.state.navBarActiveClass}`}>
                 <div className="navbar-start has-text-centered">
                   <Link className="navbar-item" to="/request-supplies">
@@ -65,7 +63,7 @@ const Navbar = class extends React.Component {
               </div> {/* navMenu */}
             </div> {/* Container */}
             <div style={{ display: "flex",alignItems: "center", flex: "1" }}>
-              <img src={maskNowImg} alt="Blah" style={{maxHeight: "228px", width: "100%"}}></img>
+              <img src={maskNowImg} alt="Blah" class="header-banner" style={{maxHeight: "228px", width: "100%"}}></img>
             </div>
           </div>
         </div>
