296e26055933172529d8562af1a72ba048783c00
mpasalic <mirza.pasalic@gmail.com>
2020-03-26 22:32:32 -0700

    Improve header UX round #1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import logo from '../img/masksnow.svg'
+import headerBackground from "../img/header-bg.png"
+import logoImg from "../img/header-rosie.png";
+import logoImgMobile from "../img/header-mobile.png";
+import maskNowImg from "../img/header-masksnow.png"
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -34,32 +38,37 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="The Masks Now Coalition">
-              <img src={logo} alt="The Masks Now Coalition" />
-            </Link>
-            {/* Hamburger menu */}
-            <div className={`navbar-burger burger ${this.state.navBarActiveClass}`} data-target="navMenu" onClick={() => this.toggleHamburger()}>
-            </div> {/* Navbar-burger */}
-          </div> {/* Navbar-brand */}
-          <div id="navMenu" className={`navbar-menu ${this.state.navBarActiveClass}`}>
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/request-supplies">
-                Request Supplies
-              </Link>
-              <Link className="navbar-item" to="/patterns">
-                Patterns
-              </Link>
-              <Link className="navbar-item" to="/groups-directory">
-                Groups Directory
-              </Link>
-              <Link className="navbar-item" to="/volunteer">
-                Volunteer
-              </Link>
-            </div> {/* Navbar-start */}
-          </div> {/* navMenu */}
-        </div> {/* Container */}
+        <div className="header" style={{flexDirection: "row", backgroundImage: "url(" + headerBackground + ")", backgroundSize: "contain", backgroundRepeat: "repeat-x"}}>
+          <img src={logoImg} style={{height: "300px"}} alt="We can do it!"></img>
+          <div style={{flexDirection: "column", display: "flex"}}>
+            <div style={{height: "74px"}}>
+            
+              {/* Hamburger menu */}
+              <div className={`navbar-burger burger ${this.state.navBarActiveClass}`} data-target="navMenu" onClick={() => this.toggleHamburger()}>
+              </div> {/* Navbar-burger */}
+
+              <div id="navMenu" className={`navbar-menu ${this.state.navBarActiveClass}`}>
+                <div className="navbar-start has-text-centered">
+                  <Link className="navbar-item" to="/request-supplies">
+                    Request Supplies
+                  </Link>
+                  <Link className="navbar-item" to="/patterns">
+                    Patterns
+                  </Link>
+                  <Link className="navbar-item" to="/groups-directory">
+                    Groups Directory
+                  </Link>
+                  <Link className="navbar-item" to="/volunteer">
+                    Volunteer
+                  </Link>
+                </div> {/* Navbar-start */}
+              </div> {/* navMenu */}
+            </div> {/* Container */}
+            <div style={{ display: "flex",alignItems: "center", flex: "1" }}>
+              <img src={maskNowImg} alt="Blah" style={{maxHeight: "228px", width: "100%"}}></img>
+            </div>
+          </div>
+        </div>
       </nav>
     )
   }
