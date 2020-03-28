46c1e78c6ee5678a914255ba672cfb468e34441e
Daniel Evans <me@daredoes.work>
2020-03-27 22:05:57 -0700

    remove unused package

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,6 @@ module.exports = {
               height: 473
             }
           },
-          "gatsby-remark-responsive-iframe",
           {
             resolve: "gatsby-remark-relative-images",
             options: {
