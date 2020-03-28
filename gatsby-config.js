76121bbe194467557150a3e2a7871739a2f8a656
Daniel Evans <me@daredoes.work>
2020-03-28 09:18:24 -0700

    attempt to update headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,7 +103,7 @@ module.exports = {
           "Access-Control-Allow-Origin: *",
           "X-Frame-Options: SAMEORIGIN"
         ], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeSecurityHeaders: false, // boolean to turn off the default security headers
         mergeLinkHeaders: false, // boolean to turn off the default gatsby js headers (disabled by default, until gzip is fixed for server push)
         mergeCachingHeaders: true, // boolean to turn off the default caching headers
         transformHeaders: (headers, path) => headers // optional transform for manipulating headers under each path (e.g.sorting), etc.
