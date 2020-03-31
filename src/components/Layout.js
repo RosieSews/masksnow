80ffe4abc1ebb4f63d32893ec39de8772bb04900
Josh Pollock <jpollock412@gmail.com>
2020-03-31 09:43:06 -0400

    Pass post frontmatter to TemplateWrapper and SEO component #7

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,31 +7,31 @@ import "./all.sass";
 import useSiteMetadata from "./SiteMetadata";
 import SEO from "./SEO";
 
-const TemplateWrapper = ({ children }) => {
-  const { title, description, image, siteUrl } = useSiteMetadata();
-  return (
-    <div>
-      <SEO />
-      <Helmet>
-        {/* <html lang="en" />
+const TemplateWrapper = ({ children, postNode }) => {
+	const { title, description, image, siteUrl } = useSiteMetadata();
+	return (
+		<div>
+			<SEO postNode={postNode} article={postNode ? true : false} />
+			<Helmet>
+				{/* <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={`${siteUrl}/${image}`} />
         <meta name="twitter:description" content={description} /> */}
 
-        <link
-          rel="icon"
-          type="image/png"
-          href={`${withPrefix("/")}img/favicon.png`}
-          sizes="16x16"
-        />
-      </Helmet>
-      <Navbar />
-      <div>{children}</div>
-      <Footer />
-    </div>
-  );
+				<link
+					rel="icon"
+					type="image/png"
+					href={`${withPrefix("/")}img/favicon.png`}
+					sizes="16x16"
+				/>
+			</Helmet>
+			<Navbar />
+			<div>{children}</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default TemplateWrapper;
--- src/pages/patterns/index.md
+++ src/pages/patterns/index.md
@@ -2,15 +2,17 @@
 templateKey: patterns
 path: /patterns
 title: Patterns
+description: Downloadable pattenrs for Crisis 3-Layer Mask Pattern Packet which includes instructions, pattern, tips, materials guide, process guide and packing sheet.
 ---
+
 Download Created for Crisis 3-Layer Mask Pattern Packet which includes instructions, pattern, tips, materials guide, process guide and packing sheet.
 
 We are in discussions to partner with the CDC for the 3-Layer Mask pattern. This pattern requires the use of a midweight/lightweight non-woven polyester material, for example:
 
-* Pellon 830 Easy Pattern
-* Pellon 40 Midweight Stabilizer
-* Pellon 30 Lightweight Stabilizer
-* Pellon 810
+- Pellon 830 Easy Pattern
+- Pellon 40 Midweight Stabilizer
+- Pellon 30 Lightweight Stabilizer
+- Pellon 810
 
 If you do not have access to this material, you may use the [Pocket pattern](https://masksnow.org/docs/CFC_Pocket_Mask_3_28.pdf) as a backup.
 
@@ -20,5 +22,4 @@ Smaller sizes coming soon.
 
 [![Download 3-layer mask pattern](/img/3-layer-mask-download.png "Download 3-layer mask pattern")](https://masksnow.org/docs/CFCMask_3_27.pdf)
 
-`youtube:https://www.youtube.com/embed/J0HnaWUIyzg`
-
+`youtube:https://www.youtube.com/embed/J0HnaWUIyzg`
